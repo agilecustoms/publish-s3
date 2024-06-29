12b5645b390a0a108a90c48646ce2cb6b8a0897d
Alex Chekulaev <chekulaevalexey@gmail.com>
2024-06-29 11:10:44 -0400

    print inputs

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.info('AlexC2');
+const core_1 = require("@actions/core");
+const sourceDir = (0, core_1.getInput)('source_dir', { required: true });
+const bucket = (0, core_1.getInput)('bucket', { required: true });
+console.info(`Uploading ${sourceDir} to ${bucket}`);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.info('AlexC2');
+import {getInput} from "@actions/core";
+
+const sourceDir: string = getInput('source_dir', {required: true});
+const bucket: string = getInput('bucket', {required: true});
+
+console.info(`Uploading ${sourceDir} to ${bucket}`);
