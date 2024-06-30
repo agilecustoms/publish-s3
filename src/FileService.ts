96c40077aebbc9348cdc040cd9fc395a570b6124
Alex Chekulaev <chekulaevalexey@gmail.com>
2024-06-30 08:21:50 -0400

    Minor change to test how release works

--- src/FileService.ts
+++ src/FileService.ts
@@ -13,7 +13,7 @@ export class FileService {
             }
             const contentTypeHeader = mime.contentType(contentType);
             if (contentTypeHeader === false) {
-                throw new Error(`Could not infer content type header for ${contentType}`);
+                throw new Error(`Could not infer Content-Type header for ${contentType}`);
             }
             fileInfos.push({
                 name: file,
