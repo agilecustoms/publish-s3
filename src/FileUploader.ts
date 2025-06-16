90194f9fb745a1b58bf146d5acff048556c0fe31
Alex Chekulaev <chekulaevalexey@gmail.com>
2025-06-15 22:21:57 -0400

    reshuffle two methods

--- src/FileUploader.ts
+++ src/FileUploader.ts
@@ -17,11 +17,40 @@ export class FileUploader {
     this.s3Client = s3Client
   }
 
-  public async upload(srcDir: string, bucket: string, bucketDir: string, version: string, tags: string = ''): Promise<void> {
-    const uploadPromises = bucketDirs.map(bucketDir =>
+  public async upload(srcDir: string, bucket: string, bucketDir: string, rawVersions: string, tags: string = ''): Promise<void> {
+    const versions = rawVersions.trim().split(',').map(v => v.trim())
+
+    const uploadPromises = versions.map(version =>
       this.uploadDir(srcDir, bucket, bucketDir, tags)
     )
     await Promise.all(uploadPromises)
+  }
+
+  private async uploadDir(srcDir: string, bucket: string, bucketDir: string, tags: string = ''): Promise<void> {
+    if (bucketDir.endsWith('/latest')) {
+      await this.deleteObjectsInDir(bucket, bucketDir)
+    }
+
+    core.info(`Uploading ${srcDir}/* to ${bucket}/${bucketDir}${tags ? ` with tags ${tags}` : ''}`)
+
+    const files = this.fileService.listFiles(srcDir)
+    for (const file of files) {
+      core.debug(`uploading ${file.name}`)
+      const input: PutObjectCommandInput = {
+        Bucket: bucket,
+        Key: `${bucketDir}/${file.name}`,
+        Body: this.fileService.readFile(file.fullPath),
+        ContentType: file.contentType
+      }
+      if (tags.length > 0) {
+        input.Tagging = tags
+      }
+      const output = await this.s3Client.send(new PutObjectCommand(input))
+      const statusCode = output.$metadata.httpStatusCode
+      if (statusCode !== 200) {
+        throw new Error(`Failed to upload ${file}, status code: ${statusCode}`)
+      }
+    }
   }
 
   private async deleteObjectsInDir(bucket: string, bucketDir: string): Promise<void> {
@@ -54,33 +83,6 @@ export class FileUploader {
     statusCode = deleteOutput.$metadata.httpStatusCode
     if (statusCode !== 200) {
       throw new Error(`Failed to delete ${bucket}/${bucketDir}, status code: ${statusCode}`)
-    }
-  }
-
-  private async uploadDir(srcDir: string, bucket: string, bucketDir: string, tags: string = ''): Promise<void> {
-    if (bucketDir.endsWith('/latest')) {
-      await this.deleteObjectsInDir(bucket, bucketDir)
-    }
-
-    core.info(`Uploading ${srcDir}/* to ${bucket}/${bucketDir}${tags ? ` with tags ${tags}` : ''}`)
-
-    const files = this.fileService.listFiles(srcDir)
-    for (const file of files) {
-      core.debug(`uploading ${file.name}`)
-      const input: PutObjectCommandInput = {
-        Bucket: bucket,
-        Key: `${bucketDir}/${file.name}`,
-        Body: this.fileService.readFile(file.fullPath),
-        ContentType: file.contentType
-      }
-      if (tags.length > 0) {
-        input.Tagging = tags
-      }
-      const output = await this.s3Client.send(new PutObjectCommand(input))
-      const statusCode = output.$metadata.httpStatusCode
-      if (statusCode !== 200) {
-        throw new Error(`Failed to upload ${file}, status code: ${statusCode}`)
-      }
     }
   }
 }
